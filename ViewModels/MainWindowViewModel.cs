f72a734e05bd9b664dda3f99f784fb01c5b99c88
Ivan Kravarščan <subchannel13@gmail.com>
2026-04-07 22:01:13 +0200

    Created Avalonia base project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Schamar.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Schamar.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
