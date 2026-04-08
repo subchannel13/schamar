94592e029d23b2b4d06360ada551f733ef19e019
Ivan Kravarščan <ivan.kravarscan.ext@ericsson.com>
2026-04-08 14:30:31 +0200

    Input folder browsing

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace Schamar.ViewModels;
+﻿using System;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Schamar.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty] private string _inputPath = "";
+
+    //public string InputPath { get; set; }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using Schamar.ViewModels;
 
 namespace Schamar.Views;
 
@@ -7,5 +11,16 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private async void BrowseInput_Click(object sender, RoutedEventArgs e) 
+    {
+        var folder = await StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+        {
+            Title = "Open Text File",
+            AllowMultiple = false,
+        });
+
+        (this.DataContext as MainWindowViewModel)!.InputPath = folder[0].Path.LocalPath;
     }
 }
